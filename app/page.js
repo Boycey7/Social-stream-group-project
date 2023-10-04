9d1a276d22064a4115121bb8a895f290f4c9ba06
mjjstockman <mjjstockman@gmail.com>
2023-10-04 19:44:52 +0100

    Add id's to objects

--- app/page.js
+++ app/page.js
@@ -3,16 +3,19 @@ import Main from "@/components/cards/Main";
 export default function Home() {
   const mains = [
     {
+      id: 1,
       name: "hello",
       image: "image",
       summary: "summary",
     },
-    {
+    { 
+      id: 2,
       name: "hello1",
       image: "image1",
       summary: "summary1",
     },
     {
+      id: 3,
       name: "hello2",
       image: "image2",
       summary: "summary2",
@@ -21,7 +24,6 @@ export default function Home() {
 
   return (
     <>
-      <p>sssshello</p>
       <div>
         {mains.map((main) => (
           <Main name={main.name} image={main.image} summary={main.summary} />
